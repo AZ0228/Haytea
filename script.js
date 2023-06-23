84052d04bdada736e8ef856b38512fa0d2754ba8
Isekouyo <jbliu88@gmail.com>
2023-06-22 22:30:43 -0700

    added fruit teas and yakult

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const recipes = {
     a1:{
         name: 'pink lady peach green tea sc',
         gtea: 250,
-        syrup: 'peach 50ml',
+        peach:50,
         peach: '2 pieces',
         instructions: 'blender 5 seconds',
     },
@@ -15,7 +15,7 @@ const recipes = {
     a2:{
         name: 'jasmine green tea sc',
         jtea: 300,
-        syrup: 'jasmine 30ml',
+        jasmine: 30,
         toppings: 'salted cheese',
     },
 
@@ -76,22 +76,37 @@ const recipes = {
 
     b5:{
         name: 'milk tea with pudding and grass jelly',
-        btea: 200,
-        sugar: 25,
+        tea: 'see b1',
         toppings: 'grass jelly and pudding',
     },
 
     b6:{
         name: 'jasmine milk tea',
-        jtea: 200,
-        sugar: 25,
+        tea: 'see b1',
         syrup: 'jasmine 30',
+    },
+
+    b7:{
+        name: 'milk tea with purple rice & red bean',
+        tea: 'see b1',
+        toppings: 'purple rice and red bean'
+    },
+
+    b8:{
+        name: 'milk tea with boba & crystal boba',
+        tea: 'see b1',
+        toppings: 'boba and crystal boba'
+    },
+
+    b8:{
+        name: 'pudding milk tea',
+        tea: 'see b1',
+        toppings: 'egg pudding'
     },
 
     b10:{
         name: 'cake milk tea',
-        mtea: 200,
-        sugar: 25,
+        tea: 'see b1',
         instructions: 'cream around the cup and on top of the tea, put brown sugar on top, use metal around the cup and torch, to turn on the torch, rotate knob left and press trigger, use from far away and make sure all sugar is melted',
     },
 
@@ -119,7 +134,7 @@ const recipes = {
 
     b15:{
         name: 'happy family',
-        instructions: 'either 200 ml milktea or 200 ml coconut milk',
+        instructions: 'either 200 ml b1 or 200 ml coconut milk',
     },
 
     b16:{
@@ -148,11 +163,124 @@ const recipes = {
         mtpowder: '1 spoon',
     },
 
+    c1:{
+        name: 'passion yakult',
+        gtea: 300,
+        yogurt: 60,
+        passion: 40,
+        toppings: '1 spoon fresh passion',
+    },
+
+    c2:{
+        name: 'mango yakult',
+        gtea: 300,
+        yogurt: 60,
+        mango: 40,
+        toppings: 'half spoon mango',
+    },
+
+    c3:{
+        name: 'strawberry yakult',
+        gtea: 300,
+        yogurt: 60,
+        strawberry: 40,
+        toppings: 'half spoon mango',
+    },
+
+    c4:{
+        name: 'kumquat passion yakult',
+        gtea: 300,
+        yogurt: 60,
+        passion: 25,
+        kumquat: 25,
+        toppings: 'crush kumquat and 1 spoon passion',
+    },
+
+    c5:{
+        name: 'grapefruit yakult',
+        gtea: 300,
+        yogurt: 60,
+        grapefruit: 40,
+        toppings: '1 spoon grapefruit',
+    },
+
+    d1:{
+        name: 'fruit fusion green tea',
+        gtea: 300,
+        orange: 20,
+        passion: 40,
+        sugar: 20,
+        toppings: '3 lime, 2 lemon, 1 orange, crush, watermelon, sliced strawberry',
+
+    },
+
+    d2:{
+        name: 'grapefruit green tea',
+        gtea: 300,
+        grapefruit: 50,
+        sugar: 20,
+        toppings: '1 spoon grapefruit',
+    },
+
+    d3:{
+        name: 'passion green tea',
+        gtea: 300,
+        passion: 40,
+        sugar: 20,
+        toppings: '1 spoon passion',
+
+    },
+
+    d4:{
+        name: 'kumquat green tea',
+        gtea: 300,
+        kumquat: 60,
+        sugar: 40,
+        toppings: '1 crushed kumquat',
+
+    },
+
+    d5:{
+        name: 'watermelon orange green tea',
+        gtea: 300,
+        orange: 50,
+        passion: 40,
+        sugar: 20,
+        toppings: 'half juiced orange, orange slice, watermelon cube',
+
+    },
+
+    d6:{
+        name: 'lychee orange green tea',
+        gtea: 300,
+        orange: 25,
+        lychee: 25,
+        sugar: 20,
+        toppings: 'half juiced orange',
+    },
+
+    d7:{
+        name: 'hk style tea',
+        hktea: 150,
+        water:100,
+        sugar: 70,
+        toppings: '7 lemon slices',
+
+    },
+
+    d1:{
+        name: 'hey lemon tea',
+        heylemon: 300,
+        sugar: 70,
+        instructions: 'in big cup, fill 400 ice, 3 slices lime, 3 slices, lemon, crush, add tea & shake, pour all into cup',
+    },
+
+
     h1:{
         name: 'mango dreamer slush',
         water: 100,
         sugar: 30,
-        syrup: "mango 50",
+        mango:50,
         instructions: 'add 1 spoon of ice, 1.5 for large, and 1 piece of mango. after blending, line cup with mango syrup and pour, put mango pieces on top',
     },
 
@@ -160,7 +288,7 @@ const recipes = {
         name: 'strawberry lover slush',
         water: 100,
         sugar: 30,
-        syrup: "strawberry 50",
+        strawberry: 50,
         instructions: 'add 1 spoon of ice, 1.5 for large, and 2 pieces of strawberry. after blending, line cup with mango syrup and pour, put strawberry syrup on top',
     },
 
@@ -168,7 +296,7 @@ const recipes = {
         name: 'lychee grape slush',
         water: 100,
         sugar: 30,
-        syrup: "lychee 50",
+        lychee: 50,
         instructions: 'add 1 spoon of ice, 1.5 for large, and 5 pieces of grape',
     },
 
@@ -240,6 +368,12 @@ const recipes = {
 function loadRecipe(element,name) {
     const recipeSelect = document.getElementById("recipe-select");
     const recipeDetails = document.getElementById("recipe-details");
+    if(selected == element){
+        selected.classList.remove('selected');
+        recipeDetails.innerHTML = "";
+        selected = '';
+        return;
+    }
     if(selected != ''){
         selected.classList.remove('selected');
     }
@@ -280,9 +414,17 @@ function loadRecipe(element,name) {
 function toggleDrop(element) {
     let parent = element.parentNode;
     let drop = parent.querySelector('.options');
+    let arrow = element.querySelector('.down');
     if (drop.classList.contains('visible')) {
         drop.classList.remove('visible')
     } else {
         drop.classList.add('visible')
+    }
+    if(arrow.classList.contains('up')){
+        console.log(1);
+        arrow.classList.remove('up');
+    } else {
+        console.log(2);
+        arrow.classList.add('up');
     }
 }
